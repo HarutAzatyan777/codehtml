39c88c9aae74d5b5f821ad13a6593015f74a7a73
sky <harutazatyan45@gmail.com>
2024-03-04 15:37:53 +0400

    04.03.2024

---
+++ src/routes.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Routes, Route} from 'react-router-dom';
+
+
+
+// Import components
+import Home from './pages/Home/Home';
+
+const RoutesConfig = () => {
+  
+  
+   
+    return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+
+      </Routes>
+  );
+};
+
+export default RoutesConfig;
