aa8a75a574ba3d06e7fd25c4d424d9e7c47c3f47
HarutAzatyan777 <harutazatyan45@gmail.com>
2024-03-15 01:44:02 +0400

    14.03.2024-2

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+
 
 import App from './App';
 import './index.css';
 
+ReactDOM.render(
 
-const root = createRoot(document.getElementById('root'));
-root.render(
-  <Router>
-    <App />
-  </Router>
+    <Router>
+      <App />
+    </Router>
+,
+  document.getElementById('root')
 );
