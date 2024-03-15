a0017155cbae91b19fb1bd95fe7886bc9179e694
sky <harutazatyan45@gmail.com>
2024-03-15 10:58:44 +0400

    15.03.2024-1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 
 import './App.css';
+import React, { useEffect } from 'react';
 import RoutesConfig from './routes';
 import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header';
+import ReactGA from 'react-ga';
 
 function App() {
+
+  useEffect(() => {
+    // Initialize Google Analytics
+    ReactGA.initialize('G-G2KGKYPNEV');
+
+    // Track page view
+    ReactGA.pageview(window.location.pathname + window.location.search);
+
+  }, []);
   return (
     <div className="App">
       <Header />
---
+++ src/components/GoogleAnalytic/GoogleAnalytic.js
@@ -0,0 +1,48 @@
+import { useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+function GoogleAnalytics() {
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        
+        const gtag = window.gtag;
+
+        if (!gtag) {
+            // Load Google Analytics script dynamically
+            const script = document.createElement('script');
+            script.async = true;
+            script.src = `https://www.googletagmanager.com/gtag/js?id=G-G2KGKYPNEV`;
+
+            script.onload = () => {
+                window.dataLayer = window.dataLayer || [];
+                function gtag() {
+                    window.dataLayer.push(arguments);
+                }
+                gtag('js', new Date());
+                gtag('config', 'G-G2KGKYPNEV');
+            };
+
+            document.head.appendChild(script);
+            return;
+        }
+
+        // Google Analytics tracking code
+        try {
+            if (location.pathname !== window.location.pathname && navigate.isPush) {
+                gtag('config', 'G-G2KGKYPNEV', {
+                    'page_title': document.title,
+                    'page_location': window.location.href,
+                    'page_path': location.pathname,
+                });
+            }
+        } catch (error) {
+            console.error('Error in Google Analytics tracking:', error);
+        }
+    }, [location, navigate]);
+
+    return null;
+}
+
+export default GoogleAnalytics;
