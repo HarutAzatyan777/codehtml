6edce42b4171898dd0fa727743622231a30ad45d
HarutAzatyan777 <harutazatyan45@gmail.com>
2024-03-18 21:55:33 +0400

    18.03.2024-4

--- src/components/Project/ProjectCard.jsx
+++ src/components/Project/ProjectCard.jsx
@@ -4,12 +4,15 @@ import "./ProjectCard.css"
 const ProjectCard = ({ title, description, imageUrl, projectUrl }) => {
   return (
     <div className="project-card">
+      <a href={projectUrl} className='a-img'>
       <img src={imageUrl} alt={title} className="project-image" />
+
+      </a>
       <div className="project-details">
+     
+        <a href={projectUrl} className="project-link"  rel="noopener noreferrer">
         <h2 className="project-title">{title}</h2>
         <p className="project-description">{description}</p>
-        <a href={projectUrl} className="project-link" target="_blank" rel="noopener noreferrer">
-          View Project
         </a>
       </div>
     </div>
