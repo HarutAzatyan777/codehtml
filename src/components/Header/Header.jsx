ce458404dc55da53cfc6d24f0ce27a2c54530f61
sky <harutazatyan45@gmail.com>
2024-03-27 14:49:26 +0400

    26.03.2024-2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,11 +9,11 @@ const Header = () => {
     setMobileMenuOpen(prevState => !prevState);
   };
   
-
-  // Function to close mobile menu when clicking outside of it
+  // Function to close mobile menu when clicking outside of it or on an <li> element
   const closeMobileMenu = (event) => {
     const isMenuClicked = event.target.closest('.main-navigation') !== null;
-    if (!isMenuClicked && mobileMenuOpen) {
+    const isListItemClicked = event.target.closest('.main-navigation ul li') !== null;
+    if ((!isMenuClicked || isListItemClicked) && mobileMenuOpen) {
       setMobileMenuOpen(false);
     }
   };
@@ -32,48 +32,40 @@ const Header = () => {
     <header className={`main-header ${mobileMenuOpen ? 'mobile-menu-open' : ''}`}>
       <div className="logo-container">
         <NavLink to="/">
-        <img src="Logo1.svg" alt="Logo" className="logo" title="HTML Tools: Color Generator, Tags, Courses, and Trending Colors | HTML School" />
-
+          <img src="Logo1.svg" alt="Logo" className="logo" title="HTML Tools: Color Generator, Tags, Courses, and Trending Colors | HTML School" />
         </NavLink>
       </div>
       <nav className={`main-navigation ${mobileMenuOpen ? 'mobile-menu-open' : ''}`}>
-      <div className="menu-icon" onClick={toggleMobileMenu}>
-  <div className="bar"></div>
-  <div className="bar"></div>
-  <div className="bar"></div>
-</div>
-
+        <div className="menu-icon" onClick={toggleMobileMenu}>
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
         <ul>
           <li>
-            <NavLink to="/HtmlTag">HTML Tags</NavLink>
+            <NavLink to="/HtmlTag" onClick={closeMobileMenu}>HTML Tags</NavLink>
           </li>
           <li>
-            <NavLink to="/ColorGenerator">Color Generator</NavLink>
+            <NavLink to="/ColorGenerator" onClick={closeMobileMenu}>Color Generator</NavLink>
           </li>
           <li>
-            <NavLink to="/TrendingColor">Trending Color</NavLink>
-          </li>
-
-          <li>
-            <NavLink to="/ImageGenerator">  Image Generator</NavLink>
+            <NavLink to="/TrendingColor" onClick={closeMobileMenu}>Trending Color</NavLink>
           </li>
           <li>
-            <NavLink to="/HTMLCourses">HTML Courses</NavLink>
-          </li>
-        
-        
-
-          <li>
-            <NavLink to="/Resources"> Dev Resources</NavLink>
+            <NavLink to="/ImageGenerator" onClick={closeMobileMenu}>Image Generator</NavLink>
           </li>
           <li>
-            <NavLink to="/programingblog">Blog</NavLink>
+            <NavLink to="/HTMLCourses" onClick={closeMobileMenu}>HTML Courses</NavLink>
           </li>
           <li>
-            <NavLink to="/login">Log In</NavLink>
+            <NavLink to="/Resources" onClick={closeMobileMenu}>Dev Resources</NavLink>
           </li>
-        
-        
+          <li>
+            <NavLink to="/programingblog" onClick={closeMobileMenu}>Blog</NavLink>
+          </li>
+          <li>
+            <NavLink to="/login" onClick={closeMobileMenu}>Log In</NavLink>
+          </li>
         </ul>
       </nav>
     </header>
