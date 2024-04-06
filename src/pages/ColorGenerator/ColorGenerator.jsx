c1d75f52eaecab8099d5fc632e102a4db31d0f4c
sky <harutazatyan45@gmail.com>
2024-04-06 11:53:20 +0400

    06.04.2024

--- src/pages/ColorGenerator/ColorGenerator.jsx
+++ src/pages/ColorGenerator/ColorGenerator.jsx
@@ -1,16 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import html2canvas from 'html2canvas';
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
-import './ColorGenerator.css';
+import React, { useState, useEffect } from "react";
+import html2canvas from "html2canvas";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import "./ColorGenerator.css";
 
 const ColorGenerator = () => {
   const [selectedColors, setSelectedColors] = useState([
-    '#03045e',
-    '#023e8a',
-    '#0077b6',
-    '#0096c7',
-    '#00b4d8',
-    '#caf0f8'
+    "#03045e",
+    "#023e8a",
+    "#0077b6",
+    "#0096c7",
+    "#00b4d8",
+    "#caf0f8",
   ]);
   const [hoveredIndex, setHoveredIndex] = useState(null);
   const [isMobile, setIsMobile] = useState(false);
@@ -21,10 +21,10 @@ const ColorGenerator = () => {
     };
 
     handleResize(); // Initial check
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
 
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
@@ -40,18 +40,20 @@ const ColorGenerator = () => {
   };
 
   const handleScreenshot = () => {
-    const colorSection = document.querySelector('.color-section');
-    html2canvas(colorSection).then(canvas => {
-      const image = canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream');
-      const link = document.createElement('a');
-      link.download = 'color_palette.png';
+    const colorSection = document.querySelector(".color-section");
+    html2canvas(colorSection).then((canvas) => {
+      const image = canvas
+        .toDataURL("image/png")
+        .replace("image/png", "image/octet-stream");
+      const link = document.createElement("a");
+      link.download = "color_palette.png";
       link.href = image;
       link.click();
     });
   };
 
   const generateRandomColor = () => {
-    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+    return "#" + Math.floor(Math.random() * 16777215).toString(16);
   };
 
   const addColor = () => {
@@ -76,43 +78,104 @@ const ColorGenerator = () => {
   };
 
   return (
-    <div className='color-generator-container'>
+    <div className="color-generator-container">
       <DragDropContext onDragEnd={onDragEnd}>
         {isMobile ? (
           <Droppable droppableId="color-section" direction="vertical">
             {(provided) => (
               <div
-                className='color-section'
+                className="color-section"
                 ref={provided.innerRef}
                 {...provided.droppableProps}
               >
                 {selectedColors.map((color, index) => (
-                  <Draggable key={index} draggableId={`color-${index}`} index={index}>
+                  <Draggable
+                    key={index}
+                    draggableId={`color-${index}`}
+                    index={index}
+                  >
                     {(provided) => (
                       <div
                         ref={provided.innerRef}
                         {...provided.draggableProps}
                         {...provided.dragHandleProps}
-                        className='color-generator'
+                        className="color-generator"
                         onMouseEnter={() => setHoveredIndex(index)}
                         onMouseLeave={() => setHoveredIndex(null)}
                       >
-                        <div className='color-preview' style={{ backgroundColor: color }}>
-                          <h1 className='color-title'>{color}</h1>
+                        <div
+                          className="color-preview"
+                          style={{ backgroundColor: color }}
+                        >
                           {hoveredIndex === index && (
                             <div className="color-menu">
-                              <button className='remove-color-button' onClick={() => removeColor(index)}>
-                                Remove
-                              </button>
-                              <button className='copy-button' onClick={() => handleCopyToClipboard(index)}>
-                                Copy
-                              </button>
-                              <input
-                                className='color-input'
-                                type="color"
-                                value={selectedColors[index]}
-                                onChange={(event) => updateRgbValues(index, event.target.value)}
-                              />
+                              <div
+                                className="add-color-button"
+                                onClick={addColor}
+                              >
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  fill="none"
+                                  viewBox="0 0 24 24"
+                                  stroke-width="1.5"
+                                  stroke="currentColor"
+                                  class="w-6 h-6"
+                                >
+                                  <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m3.75 9v6m3-3H9m1.5-12H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
+                                  />
+                                </svg>
+                              </div>
+                              <div
+                                className="remove-color-button"
+                                onClick={() => removeColor(index)}
+                              >
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  fill="none"
+                                  viewBox="0 0 24 24"
+                                  stroke-width="1.5"
+                                  stroke="currentColor"
+                                  class="w-6 h-6"
+                                >
+                                  <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                                  />
+                                </svg>
+                              </div>
+                              <div
+                                className="copy-button"
+                                onClick={() => handleCopyToClipboard(index)}
+                              >
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  fill="none"
+                                  viewBox="0 0 24 24"
+                                  stroke-width="1.5"
+                                  stroke="currentColor"
+                                  class="w-6 h-6"
+                                >
+                                  <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75"
+                                  />
+                                </svg>
+                              </div>
+                              <div className="color-input-container">
+                                <input
+                                  className="color-input"
+                                  type="color"
+                                  value={selectedColors[index]}
+                                  onChange={(event) =>
+                                    updateRgbValues(index, event.target.value)
+                                  }
+                                />
+                              </div>
                             </div>
                           )}
                         </div>
@@ -128,36 +191,105 @@ const ColorGenerator = () => {
           <Droppable droppableId="color-section" direction="horizontal">
             {(provided) => (
               <div
-                className='color-section'
+                className="color-section"
                 ref={provided.innerRef}
                 {...provided.droppableProps}
               >
                 {selectedColors.map((color, index) => (
-                  <Draggable key={index} draggableId={`color-${index}`} index={index}>
+                  <Draggable
+                    key={index}
+                    draggableId={`color-${index}`}
+                    index={index}
+                  >
                     {(provided) => (
                       <div
                         ref={provided.innerRef}
                         {...provided.draggableProps}
                         {...provided.dragHandleProps}
-                        className='color-generator'
+                        className="color-generator"
                         onMouseEnter={() => setHoveredIndex(index)}
                         onMouseLeave={() => setHoveredIndex(null)}
                       >
-                        <div className='color-preview' style={{ backgroundColor: color }}>
-                          <h1 className='color-title'>{color}</h1>
+                        <div
+                          className="color-preview"
+                          style={{ backgroundColor: color }}
+                        >
+                          <h1 className="color-title">{color}</h1>
                           {hoveredIndex === index && (
                             <div className="color-menu">
-                              <button className='remove-color-button' onClick={() => removeColor(index)}>
-                                Remove
-                              </button>
-                              <button className='copy-button' onClick={() => handleCopyToClipboard(index)}>
-                                Copy
-                              </button>
+                              <div
+                                className="add-color-button"
+                                onClick={addColor}
+                              >
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  fill="none"
+                                  viewBox="0 0 24 24"
+                                  stroke-width="1.5"
+                                  stroke="currentColor"
+                                  class="w-6 h-6"
+                                >
+                                  <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m3.75 9v6m3-3H9m1.5-12H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
+                                  />
+                                </svg>
+                              </div>
+                              <div
+                                className="remove-color-button"
+                                onClick={() => removeColor(index)}
+                              >
+                                <div
+                                  className="remove-color-button"
+                                  onClick={() => removeColor(index)}
+                                >
+                                  <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke-width="1.5"
+                                    stroke="currentColor"
+                                    class="w-6 h-6"
+                                  >
+                                    <path
+                                      stroke-linecap="round"
+                                      stroke-linejoin="round"
+                                      d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                                    />
+                                  </svg>
+                                </div>
+                              </div>
+                              <div
+                                className="copy-button"
+                                onClick={() => handleCopyToClipboard(index)}
+                              >
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  fill="none"
+                                  viewBox="0 0 24 24"
+                                  stroke-width="1.5"
+                                  stroke="currentColor"
+                                  class="w-6 h-6"
+                                >
+                                  <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75"
+                                  />
+                                </svg>
+                              </div>
                               <input
-                                className='color-input'
+                                className="color-input"
                                 type="color"
                                 value={selectedColors[index]}
-                                onChange={(event) => updateRgbValues(index, event.target.value)}
+                                onChange={(event) =>
+                                  updateRgbValues(
+                                    index,
+                                    event.target.value,
+                                    color
+                                  )
+                                }
                               />
                             </div>
                           )}
@@ -172,11 +304,8 @@ const ColorGenerator = () => {
           </Droppable>
         )}
       </DragDropContext>
-      <button className='screenshot-button' onClick={handleScreenshot}>
+      <button className="screenshot-button" onClick={handleScreenshot}>
         Screenshot
-      </button>
-      <button className='add-color-button' onClick={addColor}>
-        Add Color
       </button>
     </div>
   );
